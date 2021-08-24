5dec8a1c7c16db5b01144a73805b8055bd6375a1
0dinp0din <75302216+0dinp0din@users.noreply.github.com>
2021-08-24 10:05:55 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,17 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+
+client.on("ready", () => {
+  console.log(`logged in as ${client.user.tag}`)
+});
+
+client.on("message", msg => {
+  if (msg.content === "ping") {
+    msg.reply("pong")
+  }
+});
+const mySecret = process.env['TOKEN']
+
+
+console.log(mySecret);
+client.login(mySecret);
